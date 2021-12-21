a1d4e4f79dd810dd106f5f58f27eeb323125e572
Danesh-red-45 <86645446+Danesh-red-45@users.noreply.github.com>
2021-12-21 07:02:37 +0530

    size_datatypes_cpp

---
+++ size_op.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"size of char:"<<sizeof(char)<<"byte\n";
+    cout<<"size of float:"<<sizeof(float)<<"bytes\n";
+    cout<<"size of double:"<<sizeof(double)<<"bytes";
+}
